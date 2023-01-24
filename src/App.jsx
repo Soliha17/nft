3d1b37e93474a44c9ffdb7406aaa04a7cd57ce2b
Soliha <solyatursuntasheva@gmail.com>
2023-01-24 16:21:58 +0500

    Start. Make Header component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import "./App.scss";
+
+import Header from './components/layout/header/Index';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/header/Index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+import "./style.scss";
+
+import Logo from "../../../assets/images/logo.svg";
+import Menu from "../../../assets/icons/menu-3.png";
+
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(true);
+
+  function showMenu() {
+    setMobileMenu(!mobileMenu);
+  }
+
+  return (
+    <header className="header container">
+      <div className="header__logo">
+        <img src={Logo} alt="nft gallery logo" />
+      </div>
+      <ul className={mobileMenu ? "header__list" : "header__list--none"}>
+        <li className="header__item">
+          <a href="" className="header__link font-style--1821">
+            Home
+          </a>
+        </li>
+        <li className="header__item">
+          <a href="" className="header__link font-style--1821">
+            Activity
+          </a>
+        </li>
+        <li className="header__item">
+          <a href="" className="header__link font-style--1821">
+            Category
+          </a>
+        </li>
+        <li className="header__item">
+          <a href="" className="header__link font-style--1821">
+            Blogs
+          </a>
+        </li>
+        <li className="header__item">
+          <a href="" className="header__link font-style--1821">
+            Browser
+          </a>
+        </li>
+      </ul>
+      <button className="header__btn font-style--1619">Start Collecting</button>
+      <button className="hamburger__btn" onClick={showMenu}>
+        <img src={Menu} alt="" width="50" height="50" />
+      </button>
+    </header>
+  );
+};
+
+export default Header;
