55986a1ab8455d334fa91f3002f3330436108c61
Soliha <solyatursuntasheva@gmail.com>
2023-01-29 22:35:52 +0500

    Add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 import "./App.scss";
 
-import ReactLoading from "react-loading";
-
 import ScrollToTop from "react-scroll-to-top";
 
-import About from './components/layout/about/Index';
-import Auction from './components/layout/auction/Index';
-import Blog from './components/layout/blog/Index';
-import Category from './components/layout/category/Index';
-import Collection from './components/layout/collection/Index';
-import Create from './components/layout/create/Index';
-import Footer from './components/layout/footer/Index';
-import Gallery from './components/layout/gallery/Index';
+import About from "./components/layout/about/Index";
+import Auction from "./components/layout/auction/Index";
+import Blog from "./components/layout/blog/Index";
+import Category from "./components/layout/category/Index";
+import Collection from "./components/layout/collection/Index";
+import Create from "./components/layout/create/Index";
+import Footer from "./components/layout/footer/Index";
+import Gallery from "./components/layout/gallery/Index";
 
-import Hero from './components/layout/hero/Index';
-import Roadmap from './components/layout/roadmap/Index';
-import Seller from './components/layout/seller/Index';
-import Testimonials from './components/layout/testimonials/Index';
+import Hero from "./components/layout/hero/Index";
+import Roadmap from "./components/layout/roadmap/Index";
+import Seller from "./components/layout/seller/Index";
+import Testimonials from "./components/layout/testimonials/Index";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <div className="app">
-      const Example = ({(type, color)}) => (
-      <ReactLoading type={type} color={color} height={667} width={375} />
-      );
-      <ScrollToTop />
-      <Hero />
-      <About />
-      <Collection />
-      <Gallery />
-      <Category />
-      <Auction />
-      <Seller />
-      <Create />
-      <Roadmap />
-      <Testimonials />
-      <Blog />
-      <Footer />
+      {loading ? (
+        <div class="loader-container">
+          <div class="spinner"></div>
+        </div>
+      ) : (
+        <>
+          <ScrollToTop />
+          <Hero />
+          <About />
+          <Collection />
+          <Gallery />
+          <Category />
+          <Auction />
+          <Seller />
+          <Create />
+          <Roadmap />
+          <Testimonials />
+          <Blog />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
